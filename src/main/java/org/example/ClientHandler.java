0bbddd18fc9a41e6ea466cfeb31e37171d84404d
isadorabsagaz <isadora.sagaz@gmail.com>
2025-04-10 17:17:25 -0300

    feat(ClientHandler): initial implementation of MQTT commands (PUBLISH/SUBSCRIBE)

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -1,35 +1,82 @@
 package org.example;
 
+import com.google.gson.Gson;
+
 import java.io.*;
 import java.net.Socket;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ClientHandler implements Runnable {
 
     private final Socket socket;
+    private final Map<String, List<PrintWriter>> topics;
+    private final Client client;
 
-    public ClientHandler(Socket socket) {
+    public ClientHandler(Socket socket) throws IOException {
         this.socket = socket;
+        this.topics = new ConcurrentHashMap<>();
+        this.client = new Client(socket.getInetAddress());
     }
 
     @Override
     public void run() {
         try (
+                //creates buffer to read message and writer to send
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(socket.getInputStream())
                 );
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         ) {
-            String message;
-            while ((message = in.readLine()) != null) {
-                System.out.println("Received: " + message);
-                out.println("Received from client: " + message);
-            }
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            String line;
 
-            System.out.println("Client disconnected");
+            while ((line = in.readLine()) != null) {
+                Message msg = new Gson().fromJson(line, Message.class);
+
+                switch (msg.type.toUpperCase()) {
+
+                    //subscribe in the topic
+                    case "SUBSCRIBE" -> {
+                        topics.computeIfAbsent(msg.topic, k -> new CopyOnWriteArrayList<>())
+                                .add(out);
+                        client.getTopics().add(msg.topic);  //adding topic to client's list
+                        System.out.println("Client " + client.getId() + " SUBSCRIBED to topic: " + msg.topic);
+                    }
+
+                    //publish message to the topic
+                    case "PUBLISH" -> {
+                        if (client.getTopics().contains(msg.topic)) {
+                            System.out.println("Client " + client.getId() + " PUBLISHED to topic: " + msg.topic);
+                            System.out.println("Message: " + msg.payload);
+                            Message response = new Message();
+
+                            //generate message to publish in topic
+                            response.type = "MESSAGE";
+                            response.topic = msg.topic;
+                            response.payload = msg.payload;
+
+                            String json = new Gson().toJson(response);
+                            List<PrintWriter> registered = topics.get(msg.topic);
+
+                            //shows message to people subscribed in that topic
+                            if (registered != null) {
+                                for (PrintWriter register : registered) {
+                                    register.println(json);
+                                }
+                            }
+                        } else System.out.println("Client not subscribed to topic: " + msg.topic);
+                    }
+                    default -> System.out.println("Unknown command: " + msg.type);
+                }
+            }
+            System.out.println("Client " + client.getId() + " disconnected");
             socket.close();
 
         } catch (IOException e) {
-            e.printStackTrace();
+            e.fillInStackTrace();
+            throw new RuntimeException();
         }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,40 @@
 package org.example;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
-    public static final int SERVER_PORT = 5000;
+    private static final int SERVER_PORT = 5000;
+    private static final  ExecutorService pool = Executors.newCachedThreadPool();
 
     public static void main(String[] args)   {
-
-        ExecutorService pool = Executors.newCachedThreadPool();
-
+        //begins connection
         try (
                 ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
             System.out.println("Broker is listening on port " + SERVER_PORT);
 
+            //listens to clients
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client connection from " + clientSocket.getInetAddress());
+
+                //socket to threads pool
                 pool.execute(new ClientHandler(clientSocket));
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            e.fillInStackTrace();
+        } finally {
+            pool.shutdown(); //closes pool when server is closed
         }
     }
 }
